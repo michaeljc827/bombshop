db0ab0bd8e8bfc971ba3e82241c21d582fa46422
Michael <mchavez@sd.marketing>
2018-12-18 16:31:32 -0800

    Ignore Test 2

--- api/databaseconfig.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    settings: {
-        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-        user: "root",
-        password: "root",
-        database: 'BombShop',
-    }
-};
